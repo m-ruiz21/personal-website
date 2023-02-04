1b3a1cfb512fe389a0a8405a8a03ebb8a0ad103f
m-ruiz21 <38898004+m-ruiz21@users.noreply.github.com>
2023-02-03 23:44:31 -0600

    begin button

--- scripts/common.js
+++ scripts/common.js
@@ -10,7 +10,23 @@ function navbar() {
             <li><a href="projects.html"> Projects</a></li>
             <li><a href="service.html">Service</a></li>
             <li><a href="ai.html">ChatGPT</a></li>
+            <li><button id="styleButton"></button></li>
         </ul> 
         `
     document.body.appendChild(navbar);
+}
+
+function select_theme() {
+    document.getElementById("styleButton").addEventListener("click", function(){
+        let currStylesheet = localStorage.getItem("currentStylesheet");
+        if (currStylesheet === "app.css" || currStylesheet == null) {
+            localStorage.setItem("currStylesheet", "app.css");
+            document.getElementById("appStyle").setAttribute("href", "app.css");
+        } else {
+            localStorage.setItem("currStylesheet", "space.css");
+            document.getElementById("appStyle").setAttribute("href", "space.css");
+        }
+    });
+
+    document.getElementById("appStyle").setAttribute("href", localStorage.getItem("currentStylesheet"));
 }
